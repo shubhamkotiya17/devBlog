eca144b48beb4fa3c6f0c3afb59b29b8717d411f
shubham kotiya <kotiyashubh@gmail.com>
2020-03-10 20:24:25 +0530

    materialTestDatePicker

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,6 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// material modules
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatInputModule, MatButtonModule, MatSelectModule, MatIconModule , MatFormFieldModule
+} from '@angular/material';
+import {MatNativeDateModule} from '@angular/material';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +17,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
