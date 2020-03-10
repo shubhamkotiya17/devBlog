bd4aebc2257dc821d04f1905d3ce5d3ccf044d1d
shubham kotiya <kotiyashubh@gmail.com>
2020-03-10 21:30:30 +0530

    navbarCoded

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,20 +6,29 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // material modules
 import {MatDatepickerModule} from '@angular/material/datepicker';
-import { MatInputModule, MatButtonModule, MatSelectModule, MatIconModule , MatFormFieldModule
+import { MatInputModule, MatSelectModule , MatFormFieldModule
 } from '@angular/material';
-import {MatNativeDateModule} from '@angular/material';
+import {MatNativeDateModule, MatCardModule} from '@angular/material';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatButtonModule } from  '@angular/material';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PageNotFoundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    MatCardModule,
     AppRoutingModule,
     MatDatepickerModule,
     MatInputModule,
+    MatToolbarModule,MatIconModule,MatSidenavModule,MatListModule,MatButtonModule,
     MatFormFieldModule,
     MatNativeDateModule,
     BrowserAnimationsModule
